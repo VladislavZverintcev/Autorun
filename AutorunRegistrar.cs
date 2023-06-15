4fb04d2ff4663894ea33fc1d93948df47903a2c8
VladislavZverintcev <91832919+VladislavZverintcev@users.noreply.github.com>
2023-06-15 08:30:42 +0700

    Add files via upload

---
+++ AutorunRegistrar.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace AppDevTools.Addons
+{
+    /// <summary>
+    /// Provides a collection of properties and methods for registering application autorun
+    /// </summary>
+    public class AutorunRegistrar
+    {
+        #region Constants
+
+        #region Private
+        private const string DEF_KEY = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        #endregion Private
+
+        #endregion Constants
+
+        #region Properties
+
+        #region Public
+
+        #region Property for register key
+        /// <summary>
+        /// The specified register key
+        /// </summary>
+        public RegistryKey? RegistryKey { get; private set; }
+        #endregion Property for register key
+
+        #endregion Public
+
+        #endregion Properties
+
+        #region Constructors
+
+        #region Public
+        public AutorunRegistrar()
+        {
+            RegistryKey = Registry.CurrentUser.OpenSubKey(DEF_KEY, true);
+        }
+        #endregion Public
+
+        #endregion Constructors
+
+        #region Methods
+
+        #region Public
+
+        #region Method to get autorun registration
+        /// <summary>
+        /// Gets autorun registration associated with the specified name.
+        /// Returns null if the name/value doesn't exist in the registry
+        /// </summary>
+        /// <param name="appName">
+        /// Application name
+        /// </param>
+        /// <returns>
+        /// The value associated with name, or null if name is not found
+        /// </returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public object? Get(string? appName)
+        {
+            if (RegistryKey == null)
+            {
+                throw new ArgumentNullException(nameof(RegistryKey));
+            }
+
+            return RegistryKey.GetValue(appName);
+        }
+        #endregion Method to get autorun registration
+
+        #region Method to set autorun registration
+        /// <summary>
+        /// Sets autorun registration
+        /// </summary>
+        /// <param name="appName">
+        /// Application name
+        /// </param>
+        /// <param name="executablePathApp">
+        /// Path to the application executable file
+        /// </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void Set(string? appName, string executablePathApp)
+        {
+            if (RegistryKey == null)
+            {
+                throw new ArgumentNullException(nameof(RegistryKey));
+            }
+
+            if (executablePathApp == null)
+            {
+                throw new ArgumentNullException(nameof(executablePathApp));
+            }
+
+            RegistryKey.SetValue(appName, executablePathApp);
+        }
+        #endregion Method to set autorun registration
+
+        #region Method to delete autorun registration
+        /// <summary>
+        /// Deletes autorun registration
+        /// </summary>
+        /// <param name="appName">
+        /// Application name
+        /// </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void Delete(string appName)
+        {
+            if (RegistryKey == null)
+            {
+                throw new ArgumentNullException(nameof(RegistryKey));
+            }
+
+            if (appName == null)
+            {
+                throw new ArgumentNullException(nameof(appName));
+            }
+
+            RegistryKey.DeleteValue(appName);
+        }
+        #endregion Method to delete autorun registration
+
+        #endregion Public
+
+        #endregion Methods
+    }
+}
